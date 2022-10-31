60585246fb40cff0b3fb5f45a942acc7751d15f3
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-10-31 20:56:53 +0700

    setting up

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="flex h-[100vh] w-[100vw] items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#404258",
+        secondaryColor: "474E68",
+        tertiaryColor: "#9F73AB",
+      },
+    },
+  },
+  plugins: [],
+};
