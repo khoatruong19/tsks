91ae51da46a2e138177d0e6f34e73eaa42cc1765
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-11-01 17:02:00 +0700

    add sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { BookOpenIcon } from "@heroicons/react/24/solid";
 import { useAtom } from "jotai";
 import React from "react";
 import { openSidebarAtom } from "../store";
@@ -11,7 +12,23 @@ const Sidebar = () => {
       } 
      transform overflow-y-auto bg-secondaryColor duration-150 ease-linear`}
     >
-      sads
+      <div className="pt-8">
+        <h1 className="mb-5 pl-8 text-xl font-semibold text-textColor/80">
+          Collections
+        </h1>
+        <div className="withHover flex items-center gap-3 bg-gray-600 py-4 pl-8">
+          <div className="grid place-items-center rounded-md bg-slate-300 p-2">
+            <BookOpenIcon className="h-5 w-5" />
+          </div>
+          <span className="font-semibold text-textColor">School</span>
+        </div>
+        <div className="withHover flex items-center gap-3 py-4 pl-8">
+          <div className="grid place-items-center rounded-md bg-slate-300 p-2">
+            <BookOpenIcon className="h-5 w-5" />
+          </div>
+          <span className="font-semibold text-textColor">School</span>
+        </div>
+      </div>
     </div>
   );
 };
