1925be093f3eb23f06ed59f920bb88cf43b9e143
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-11-11 18:18:25 +0700

    done with show all collections and their details

--- src/pages/collections/[slug].tsx
+++ src/pages/collections/[slug].tsx
@@ -14,11 +14,22 @@ import Sidebar from "../../components/layout/Sidebar";
 import { openTaskModal } from "../../store";
 import { useEffect, useRef } from "react";
 import Loading from "../../components/layout/Loading";
+import { trpc } from "../../utils/trpc";
+import ClipLoader from "react-spinners/ClipLoader";
+import Loader from "../../components/others/Loader";
 
 const CollectionDetail = () => {
+  const router = useRouter();
+  const { data, isLoading } = trpc.collection.getCollectionBySlug.useQuery(
+    {
+      slug: router.query.slug as string,
+    },
+    {
+      enabled: router.query.slug ? true : false,
+    }
+  );
   const [_, setOpenModal] = useAtom(openTaskModal);
   const { status } = useSession();
-  const router = useRouter();
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -44,37 +55,41 @@ const CollectionDetail = () => {
               ref={containerRef}
               className="mx-auto h-[100%] w-full max-w-3xl overflow-y-scroll  pb-10 scrollbar-hide"
             >
-              <div className="pt-10">
-                <div className="mb-6 flex flex-col gap-12 overflow-hidden">
-                  <div className="flex items-center justify-between  text-textColor">
-                    <div className="flex items-center gap-4">
-                      <div className="withHover rounded-lg bg-secondaryColor p-3">
-                        <ChevronLeftIcon className="h-6 w-6 text-textColor" />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <div className="pt-10">
+                  <div className="mb-6 flex flex-col gap-12 overflow-hidden">
+                    <div className="flex items-center justify-between  text-textColor">
+                      <div className="flex items-center gap-4">
+                        <div className="withHover rounded-lg bg-secondaryColor p-3">
+                          <ChevronLeftIcon className="h-6 w-6 text-textColor" />
+                        </div>
+                        <h3 className="text-3xl font-bold">{data?.title}</h3>
                       </div>
-                      <h3 className="text-3xl font-bold">School</h3>
+                      <div>
+                        <EllipsisHorizontalIcon className="withHover h-8 w-8" />
+                      </div>
                     </div>
-                    <div>
-                      <EllipsisHorizontalIcon className="withHover h-8 w-8" />
-                    </div>
-                  </div>
 
-                  <div
-                    className="withHover flex w-full items-center gap-3 rounded-2xl border-[1px] border-secondaryColor/80
+                    <div
+                      className="withHover flex w-full items-center gap-3 rounded-2xl border-[1px] border-secondaryColor/80
                    p-3"
-                    onClick={() => setOpenModal("Add")}
-                  >
-                    <div className="w-fit rounded-lg bg-tertiaryColor p-2">
-                      <PlusIcon className="h-5 w-5 text-textColor" />
+                      onClick={() => setOpenModal("Add")}
+                    >
+                      <div className="w-fit rounded-lg bg-tertiaryColor p-2">
+                        <PlusIcon className="h-5 w-5 text-textColor" />
+                      </div>
+                      <p className="text-lg font-medium text-white/60">
+                        Add a task
+                      </p>
                     </div>
-                    <p className="text-lg font-medium text-white/60">
-                      Add a task
-                    </p>
-                  </div>
 
-                  <TodoTasksContainer />
-                  <CompletedTasksContainer />
+                    <TodoTasksContainer />
+                    <CompletedTasksContainer />
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -8,8 +8,15 @@ import CollectionCard from "../../components/collections/CollectionCard";
 import MainLayout from "../../components/layout/MainLayout";
 import { useEffect } from "react";
 import Loading from "../../components/layout/Loading";
+import { trpc } from "../../utils/trpc";
+import Loader from "../../components/others/Loader";
+import { useAtom } from "jotai";
+import { openCollectionModal } from "../../store";
 
 const Collections: NextPage = () => {
+  const { data, isLoading } =
+    trpc.collection.getAllCollectionsWithStatus.useQuery();
+  const [_, setOpenCollectionModal] = useAtom(openCollectionModal);
   const [viewMode, setViewMode] = useState<0 | 1>(0);
   const { status } = useSession();
   const router = useRouter();
@@ -26,54 +33,63 @@ const Collections: NextPage = () => {
       </Head>
       <MainLayout>
         <div className="h-[calc(100vh_-_65px)] w-full overflow-hidden">
-          <div className="mx-auto h-[100%] w-full max-w-3xl overflow-y-scroll  px-2 pb-10 scrollbar-hide md:px-0">
-            <div className="pt-10">
-              <div className="mb-6 flex flex-col gap-5 md:gap-12">
-                <div className="flex items-center justify-between  text-textColor">
-                  <h3 className="text-3xl font-bold">Collections</h3>
-                  <div>
-                    <EllipsisHorizontalIcon className="withHover h-8 w-8" />
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div className="mx-auto h-[100%] w-full max-w-3xl overflow-y-scroll  px-2 pb-10 scrollbar-hide md:px-0">
+              <div className="pt-10">
+                <div className="mb-6 flex flex-col gap-5 md:gap-12">
+                  <div className="flex items-center justify-between  text-textColor">
+                    <h3 className="text-3xl font-bold">Collections</h3>
+                    <div>
+                      <EllipsisHorizontalIcon className="withHover h-8 w-8" />
+                    </div>
                   </div>
-                </div>
 
-                <div className="mt-10 mb-5 flex items-center gap-4 md:mb-0">
-                  <button
-                    className={`${
-                      viewMode === 0
-                        ? "bg-dashboardSecondaryColor"
-                        : "border-2 border-secondaryColor bg-transparent shadow-md"
-                    } dashboardBtn`}
-                    onClick={() => viewMode !== 0 && setViewMode(0)}
-                  >
-                    Favourites
-                  </button>
-                  <button
-                    className={`${
-                      viewMode === 1
-                        ? "bg-dashboardSecondaryColor"
-                        : "border-2 border-secondaryColor bg-transparent shadow-md"
-                    } dashboardBtn`}
-                    onClick={() => viewMode !== 1 && setViewMode(1)}
-                  >
-                    All Collections
-                  </button>
-                </div>
+                  <div className="mt-10 mb-5 flex items-center gap-4 md:mb-0">
+                    <button
+                      className={`${
+                        viewMode === 0
+                          ? "bg-dashboardSecondaryColor"
+                          : "border-2 border-secondaryColor bg-transparent shadow-md"
+                      } dashboardBtn`}
+                      onClick={() => viewMode !== 0 && setViewMode(0)}
+                    >
+                      Favourites
+                    </button>
+                    <button
+                      className={`${
+                        viewMode === 1
+                          ? "bg-dashboardSecondaryColor"
+                          : "border-2 border-secondaryColor bg-transparent shadow-md"
+                      } dashboardBtn`}
+                      onClick={() => viewMode !== 1 && setViewMode(1)}
+                    >
+                      All Collections
+                    </button>
+                  </div>
 
-                <div className="grid grid-cols-2 gap-x-3 gap-y-4 md:grid-cols-3 md:gap-4 ">
-                  <CollectionCard />
-                  <CollectionCard />
-                  <CollectionCard />
-                  <CollectionCard />
-                  <div
-                    className="withHover flex h-[105px] items-center justify-center rounded-3xl
+                  <div className="grid grid-cols-2 gap-x-3 gap-y-4 md:grid-cols-3 md:gap-4 ">
+                    {data &&
+                      data.map((collection) => (
+                        <CollectionCard
+                          collection={collection}
+                          key={collection.id}
+                        />
+                      ))}
+
+                    <div
+                      className="withHover flex h-[105px] items-center justify-center rounded-3xl
                    border-[3px] border-secondaryColor text-3xl text-white/60"
-                  >
-                    +
+                      onClick={() => setOpenCollectionModal("Add")}
+                    >
+                      +
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </MainLayout>
     </>
