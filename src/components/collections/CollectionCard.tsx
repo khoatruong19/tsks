1a819306dd4882236c7679ef83b85da2b8c1300c
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-11-11 18:16:03 +0700

    display collection from api

--- src/components/collections/CollectionCard.tsx
+++ src/components/collections/CollectionCard.tsx
@@ -1,24 +1,40 @@
 import { BookOpenIcon } from "@heroicons/react/24/outline";
+import { Collection } from "@prisma/client";
 import { useRouter } from "next/router";
 import React from "react";
+import { CollectionWithTasksQuantity } from "../../types/collection.type";
 import { getCircumfence } from "../../utils/constants";
 
 const circumference = getCircumfence(15);
 
-const CollectionCard = () => {
+interface IProps {
+  collection: CollectionWithTasksQuantity;
+}
+
+const CollectionCard = ({ collection }: IProps) => {
   const router = useRouter();
+  const doneTasks = collection.tasks.filter(
+    (task) => task.done === true
+  )?.length;
   return (
     <div
       className="withHover rounded-3xl bg-secondaryColor"
-      onClick={() => router.push("/collections/asd")}
+      onClick={() => router.push(`/collections/${collection.slug}`)}
     >
       <div className=" p-4 md:p-6">
-        <div className="mb-10 grid max-w-fit place-items-center rounded-md bg-slate-300 p-2">
-          <BookOpenIcon className="h-5 w-5" />
+        <div
+          className="mb-8 grid max-w-fit place-items-center rounded-md p-1.5"
+          style={{ backgroundColor: collection.color }}
+        >
+          <p className="text-lg">{collection.icon}</p>
         </div>
-        <h1 className="mb-1.5 text-2xl font-bold text-textColor">School</h1>
+        <h1 className="mb-3  text-2xl font-bold text-textColor">
+          {collection.title}
+        </h1>
         <div className="flex items-center  justify-between">
-          <span className="text-sm text-white/60">4/8 done</span>
+          <span className="text-sm text-white/60">
+            {doneTasks}/{collection.tasks.length} done
+          </span>
           <div className="flex items-center justify-center">
             <svg className=" h-10 w-10">
               <circle
@@ -34,7 +50,12 @@ const CollectionCard = () => {
                 className=" text-tertiaryColor"
                 strokeWidth="5"
                 strokeDasharray={circumference}
-                strokeDashoffset={circumference - (10 / 20) * circumference}
+                strokeDashoffset={
+                  collection.tasks.length > 0
+                    ? circumference -
+                      (doneTasks / collection.tasks.length) * circumference
+                    : circumference
+                }
                 stroke-linecap="round"
                 stroke="currentColor"
                 fill="transparent"
