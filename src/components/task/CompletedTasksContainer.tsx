acac1fed21367b713b684dc6da8f83a985155e3a
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-11-12 20:38:09 +0700

    smaller gap between task cards

--- src/components/task/CompletedTasksContainer.tsx
+++ src/components/task/CompletedTasksContainer.tsx
@@ -29,7 +29,7 @@ const CompletedTasksContainer = () => {
       <h1 className="m-0 text-2xl font-semibold text-textColor">
         Completed - 8
       </h1>
-      <div className="flex flex-col gap-2.5 py-5">
+      <div className="flex flex-col gap-1 py-5">
         <DndContext onDragEnd={handleDragEnd}>
           <SortableContext
             items={languages}
